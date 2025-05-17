250a5d89717920cc8d78d690d66675ef6aba3115
KristiyanAleksandrov <kristiqn.aleksandrow@gmail.com>
2025-05-17 13:41:03 +0300

    Add exception handling

--- src/backend/Auth/Auth.API/Program.cs
+++ src/backend/Auth/Auth.API/Program.cs
@@ -1,3 +1,4 @@
+using Auth.API.Infrastructure.ErrorHandling;
 using Auth.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
@@ -21,6 +24,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseExceptionHandler();
 
 app.UseAuthorization();
 
