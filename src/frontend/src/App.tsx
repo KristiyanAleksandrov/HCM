9a341b7bf136025dc166e330b2b5a537f4063d95
KristiyanAleksandrov <kristiqn.aleksandrow@gmail.com>
2025-05-19 03:47:47 +0300

    Center the App

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -5,6 +5,7 @@ import PeopleListPage from './pages/PeopleListPage'
 import PersonFormPage from './pages/PersonFormPage'
 import { useAuth } from './contexts/AuthContext'
 import type { ReactNode } from 'react'
+import { Box } from '@mui/material'
 
 interface PrivateRouteProps {
   children: ReactNode
@@ -17,33 +18,42 @@ function PrivateRoute({ children}: PrivateRouteProps) {
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route
-        path="/"
-        element={
-          <PrivateRoute>
-            <PeopleListPage />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/people/new"
-        element={
-          <PrivateRoute>
-            <PersonFormPage />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/people/:id"
-        element={
-          <PrivateRoute>
-            <PersonFormPage />
-          </PrivateRoute>
-        }
-      />
-    </Routes>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center" 
+      minHeight="100vh"
+      minWidth="100vw"
+      bgcolor="#f4f6f8"
+    >
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <PeopleListPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/people/new"
+            element={
+              <PrivateRoute>
+                <PersonFormPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/people/:id"
+            element={
+              <PrivateRoute>
+                <PersonFormPage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+    </Box>
   )
 }
